292d83f2395d5c7da690b72db220d6a9f75acef9
luan fellipe <luan.fellipe@live.com>
2026-03-21 00:16:45 +0100

    remove backoffice_api

--- backoffice_api/lib/backoffice_api/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule BackofficeApi.Mailer do
-  use Swoosh.Mailer, otp_app: :backoffice_api
-end
--- backoffice_api/lib/backoffice_api/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule BackofficeApi.Repo do
-  use Ecto.Repo,
-    otp_app: :backoffice_api,
-    adapter: Ecto.Adapters.Postgres
-end
